3057c9ea9bbb8beb73e43ce80f894892cd6dfcdb
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2025-04-16 23:16:00 +0200

    portada lista 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,16 +18,16 @@
 	<!-- Contenido sobre el video -->
 	<section class="relative z-1 flex flex-col items-center h-full  text-center">
 		<h1 class="text-4xl font-bold mb-22 mt-22">Welcome To Rent&Go</h1>
-		<p class="text-2xl max-w-[320px] mb-12">
-			Descubre una nueva forma de jugar. Alquilá juegos para tus consolas favoritas o pon los tuyos en alquiler.
+		<p class="text-xl max-w-[330px] mb-12 mt-20 text-gray-700 font-bold">
+			Descubre una nueva forma de jugar. Alquila juegos para tus consolas favoritas o pon los tuyos en alquiler.
             Fácil, rápido y sin complicaciones.
 		</p>
-		<div class="mt-6 flex gap-4 ">
-			<button class="bg-yellow-600 hover:bg-blue-700 text-white px-5 py-2 rounded-xl shadow-md">
+		<div class="mt-16 flex gap-4 ">
+			<button class="bg-black hover:bg-blue-700 text-white px-5 py-2 rounded-xl shadow-md">
 				Explorar juegos
 			</button>
-			<button class="bg-white text-yellow-600 hover:bg-gray-100 px-5 py-2 rounded-xl shadow-md">
-				Publicar un juego
+			<button class="bg-white text-black hover:bg-gray-100 px-5 py-2 rounded-xl shadow-md">
+				Registrarme
 			</button>
 		</div>
 	</section>
