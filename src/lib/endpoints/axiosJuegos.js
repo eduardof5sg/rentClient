7f9746e4426ba0073d5c9425c606d264fb62d89b
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2025-04-18 02:10:54 +0200

    Añadita ruta para info del juego y modal para consola xbox 1

--- src/lib/endpoints/axiosJuegos.js
+++ src/lib/endpoints/axiosJuegos.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const apiJuegos = axios.create({
-    // baseURL:'https://rentserver-c73g.onrender.com/juegos',
-    baseURL:'http://localhost:3000/juegos',
+    baseURL:'https://rentserver-c73g.onrender.com/juegos',
+    // baseURL:'http://localhost:3000/juegos',
     timeout: 5000,
     headers:{
         'Content-Type':'application/json'
