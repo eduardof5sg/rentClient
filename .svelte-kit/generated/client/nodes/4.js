a2fcdef8c9a8abd3cc94bd078bdb6b48436bee3c
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2025-04-21 23:24:24 +0200

    nose

---
+++ .svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/guarida/[id]/+page.js";
+export { universal };
+export { default as component } from "../../../../src/routes/guarida/[id]/+page.svelte";
---
+++ .svelte-kit/generated/client/nodes/5.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/juegos/detalles/[id]/+page.js";
+export { universal };
+export { default as component } from "../../../../src/routes/juegos/detalles/[id]/+page.svelte";
