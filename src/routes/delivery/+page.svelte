0782b4176a6af0c3ab7bae19b65c70e431e32bcd
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2025-04-21 09:34:10 +0200

    frontend historial repartidor

--- src/routes/delivery/+page.svelte
+++ src/routes/delivery/+page.svelte
@@ -1,11 +1,14 @@
 <script>
     import "../../app.css"
+    import Navbar from "$lib/componentes/navbar.svelte";
     import { onMount } from 'svelte';
     import apiDelivery from '$lib/endpoints/axiosDelivery'; // Asegúrate de importar el archivo con tu configuración de axios
   
     let alquileres = []; // Aquí guardaremos los alquileres que obtenemos del backend
+    let historiales =[];
     let error = ''; // Para manejar posibles errores
     let modalConfirmar = false
+    let modalHistorial = false
     let alquilerSeleccionado = null;
 
     const abrirModal = (alquiler)=>{
@@ -14,9 +17,17 @@
       modalConfirmar = true;
     }
 
-   
     function cerrarModal (){
       modalConfirmar = false
+    }
+
+    const abrirModalHistorial= () =>{
+      modalHistorial = true;
+      
+    }
+
+    const cerrarModalHistorial = () =>{
+      modalHistorial = false;
     }
     // Función para obtener alquileres según el código postal
     const obtenerAlquileres = async () => {
@@ -45,11 +56,8 @@
     };
 
     const estadoReparto = async (alquilerId) => {
-  try {
+    try {
     const token = localStorage.getItem('token'); // Obtenemos el token desde localStorage
-    console.log("🚀 Token:", token); // Asegúrate de que el token se está obteniendo correctamente
-
-    // Asegúrate de que el token esté presente
     if (!token) {
       error = "No se encontró el token de autenticación.";
       return;
@@ -73,23 +81,53 @@
     console.error("❌ Error al cambiar el estado:", err); // Detalles del error
     error = 'Error al cambiar a modo reparto';
   }
-};
+    };
+    
+    const historialPedidos = async() =>{
+      try {
+        const token = localStorage.getItem('token'); // Obtenemos el token desde localStorage
+  
+        // Asegúrate de que el token esté presente
+        if (!token) {
+          error = "No se encontró el token de autenticación.";
+          return;
+        }
+  
+        // Realizamos la solicitud con el token en la cabecera
+        const response = await apiDelivery.get('/historial', {
+          headers: {
+            Authorization: `Bearer ${token}`, // Enviamos el token en el header 'Authorization'
+          },
+        });
+        
+        historiales = response.data.reparto; // Guardamos los alquileres en el array
+        console.log(historiales)
+      } catch (err) {
+        error = 'Error al obtener los alquileres.'; // Manejo de errores
+        // Mostrar detalles del error
+      }
+    }
   
     // Ejecutar obtenerAlquileres cuando el componente se monte
     onMount(() => {
       obtenerAlquileres();
+      historialPedidos();
     });
   </script>
   
-  <main class="p-8 bg-gray-50 min-h-screen">
-    <h1 class="text-3xl font-bold mb-6 text-center text-gray-800">Alquileres Confirmados</h1>
-  
+  <main class=" bg-gray-50 min-h-screen">
+    <Navbar />
+    <div class="flex flex-col items-center">
+      <h1 class="text-3xl font-bold mb-4 mt-4 text-center text-gray-800">Alquileres Confirmados</h1>
+    <button on:click={() =>abrirModalHistorial()} class="p-2 bg-violet-600 rounded-xl shadow-md text-white">Historial de pedidos</button>
+    </div>
+    
     {#if error}
       <p class="text-red-600 text-center mb-4">{error}</p>
     {/if}
   
     {#if alquileres.length === 0}
-      <p class="text-center text-gray-600">No se encontraron alquileres.</p>
+      <p class="text-center text-gray-600 mt-4">No se encontraron alquileres.</p>
     {:else}
       <div class="overflow-x-auto">
         <table class="min-w-full table-auto bg-white rounded-lg shadow-md border border-gray-200">
@@ -139,6 +177,52 @@
         {/if}
       </div>
     {/if}
+    {#if modalHistorial}
+    <div class="fixed inset-0 bg-black/50 z-50 flex items-center justify-center p-4">
+      <div class="bg-white rounded-lg shadow-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto p-6 space-y-4">
+        <div class="flex flex-row justify-around">
+          <h2 class="text-xl text-center bg-violet-700 text-white rounded-xl p-2">Historial de pedidos</h2>
+          <button class="absolute top-10 right-6 text-white" on:click={() =>cerrarModalHistorial()}
+        >✖</button
+      >
+        </div>
+        {#each historiales as historial}
+          <div class="bg-gray-100 shadow rounded-md p-4">
+            <h2 class="text-lg font-bold mb-2 text-gray-800">Detalles del pedido</h2>
+            <div class="space-y-2">
+              <div class="flex justify-between border-b pb-1">
+                <span class="font-semibold text-gray-600">Juego:</span>
+                <span>{historial.juegoid}</span>
+              </div>
+              <div class="flex justify-between border-b pb-1">
+                <span class="font-semibold text-gray-600">Estado:</span>
+                <span class="text-violet-700">{historial.estado}</span>
+              </div>
+              <div class="flex justify-between border-b pb-1">
+                <span class="font-semibold text-gray-600">Fecha:</span>
+                <span>{historial.fechasolicitud}</span>
+              </div>
+              {#if historial.fechainicio && historial.fechafin}
+                <div class="flex justify-between border-b pb-1">
+                  <span class="font-semibold text-gray-600">Inicio:</span>
+                  <span>{historial.fechainicio}</span>
+                </div>
+                <div class="flex justify-between border-b pb-1">
+                  <span class="font-semibold text-gray-600">Fin:</span>
+                  <span>{historial.fechafin}</span>
+                </div>
+              {/if}
+              <div class="flex justify-between">
+                <span class="font-semibold text-gray-600">Precio:</span>
+                <span>{historial.preciofinal}€</span>
+              </div>
+            </div>
+          </div>
+        {/each}
+      </div>
+    </div>
+  {/if}
+   
   </main>
   
   
